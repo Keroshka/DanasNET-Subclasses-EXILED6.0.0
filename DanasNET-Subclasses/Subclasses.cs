259f7b98c06208f22b843ecc5f74eb51ce842246
ikerovec20 <ikerovec20@foi.hr>
2023-04-18 13:43:15 +0200

    Add project files.

---
+++ DanasNET-Subclasses/Subclasses.cs
@@ -0,0 +1,33 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanasNET_Subclasses
+{
+    public class Subclasses : Plugin<Config>
+    {
+
+        public override string Author => "Keroshka";
+        public override string Name => "DanasNET-Subclasses";
+        public override Version RequiredExiledVersion => new Version(6, 0, 0);
+
+        public Subclasses Singleton => Singleton;
+
+        public override void OnEnabled()
+        {
+
+
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+
+
+            base.OnDisabled();
+        }
+    }
+}
